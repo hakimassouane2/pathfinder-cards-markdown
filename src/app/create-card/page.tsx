4799ea3d11ae0bd781c00cfb0a5c72a278215dd9
hakimassouane2 <hakimassouane@hotmail.com>
2025-07-08 14:46:50 +0900

    feat: merged import card and create card

--- src/app/create-card/page.tsx
+++ src/app/create-card/page.tsx
@@ -1,42 +1,8 @@
 "use client";
 
-import Card from "@/components/Card";
-import CardEditFields from "@/components/CardEdit/CardEditFields";
-import { useAuth } from "@/components/FirebaseAuthProvider";
+import CardEdit from "@/components/CardEdit/CardEdit";
 import { emptyCard } from "@/data/emptyCard";
-import { useProjectActions } from "@/stores/projectStore";
-import { PageColumn, PrimaryButton } from "@/styles/commonStyledComponents";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import * as S from "./styles";
 
 export default function CreateCard() {
-  const [cardData, setCardData] = useState<CardData>(emptyCard);
-  const { addCard } = useProjectActions();
-  const { user } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!user) {
-      router.push("/");
-    }
-    // eslint-disable-next-line
-  }, [user]);
-
-  const handleAddCard = async () => {
-    await addCard(cardData);
-    setCardData(emptyCard);
-  };
-
-  return (
-    <S.CreateCardView>
-      <PageColumn>
-        <CardEditFields cardData={cardData} onSaveCardData={setCardData} />
-      </PageColumn>
-      <PageColumn>
-        <Card cardData={cardData} />
-        <PrimaryButton onClick={handleAddCard}>Sauvegarder</PrimaryButton>
-      </PageColumn>
-    </S.CreateCardView>
-  );
+  return <CardEdit initialCard={emptyCard} cardIndex={null} />;
 }
